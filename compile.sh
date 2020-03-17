307764a79b131bc24ae3175a4538502d4c3a7bba
Hsuan <hsuan.dev@gmail.com>
2020-03-17 10:27:41 +0800

    First Commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  compile.sh
+#  Created by student on 2020/3/17.
+#  Copyright © 2020 hsuan. All rights reserved.
+#  Using MIT License
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InformationClass
+//
+//  Created by student on 2020/3/17.
+//  Copyright © 2020 hsuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
