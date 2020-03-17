33e4ff34d2822a0e9111bf763b1eab1e7892e40a
Hsuan <hsuan.dev@gmail.com>
2020-03-17 11:46:35 +0800

    Add VPL

--- main.cpp
+++ main.cpp
@@ -17,7 +17,9 @@ int main(int argc, const char * argv[]) {
     if(b<60)b*=1.25;
     if(c<60)c*=1.25;
 
-    if((a+b+c)/3 < 60){
+    if(((a+b+c)/3<0)) || ((a+b+c)/3 > 100)){
+	cout<< "ReCheckScores" << endl;
+    }else if((a+b+c)/3 < 60){
         cout << "PleaseGoToDieOneDie" << endl;
     }else{
         cout << "Hmm" << endl;
